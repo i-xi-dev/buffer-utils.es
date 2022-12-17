e99e63c8585b98054be65dfd3edaf2226999111a
i-xi-dev <naoyuki.isono@gmail.com>
2022-12-17 10:38:27 +0900

    1.1.1

--- src/buffer_utils.ts
+++ src/buffer_utils.ts
@@ -1,7 +1,7 @@
 import {
   Uint8,
   type uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.1.1/mod.ts";
+} from "i-xi-dev/int.es";
 
 namespace BufferUtils {
   export type TypedArrayConstructor =
