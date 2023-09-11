c992ac5ce6d363a5baf45930f09dde9dadcd4499
i-xi-dev <naoyuki.isono@gmail.com>
2023-09-11 23:11:15 +0900

    1.1.12

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.0/mod.ts";
+export { Uint8 } from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.1/mod.ts";
