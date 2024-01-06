1cfda283f3be6ac94b399a7393cb83d513460767
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-06 23:00:05 +0900

    4.0.5

--- deps.ts
+++ deps.ts
@@ -2,4 +2,4 @@ export {
   Uint16,
   Uint32,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.1/mod.ts";
