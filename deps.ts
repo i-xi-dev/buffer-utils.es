682ceb29f921c03b19ee2b2b5aa9cc891aa9a4cf
i-xi-dev <naoyuki.isono@gmail.com>
2024-06-12 21:16:03 +0900

    6.2.4

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ export {
   Uint16,
   Uint32,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.0/mod.ts";
-export { ObjectEx } from "https://raw.githubusercontent.com/i-xi-dev/object.es/1.0.1/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/8.0.1/mod.ts";
+export { ObjectEx } from "https://raw.githubusercontent.com/i-xi-dev/object.es/1.0.2/mod.ts";
